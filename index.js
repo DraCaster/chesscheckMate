a4755a37475276c4da24e01f8b0082d32d8ad9d0
dracaster <lujanrojas.informatica@gmail.com>
2021-03-12 17:59:05 -0300

    parcialmente listo

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-const challengeFunction = function(kingWhite, rookBlack, bishopBlack, knightBlack){
-    /* YOUR CODE HERE */
-    let result
+const challengeFunction = function (kingWhite, rookBlack, bishopBlack, knightBlack) {
 
-    return result
+    if (obtenerMovimientoAlfil(bishopBlack, kingWhite) && obtenerMovimientoCaballo(knightBlack, kingWhite)
+        && obtenerMovimientoTorre(rookBlack, kingWhite)) {
+        return "jaque mate"
+    }
+    return "jaque"
 }
 
 module.exports = challengeFunction
+
+
+function obtenerMovimientoAlfil(alfil, rey) {
+    let posRey = rey.split("")
+    return true
+}
+
+function obtenerMovimientoTorre(torre, rey) {
+    let posRey = rey.split("")
+    return true
+}
+
+function obtenerMovimientoCaballo(caballo, rey) {
+    let posRey = rey.split("")
+    return true
+}
